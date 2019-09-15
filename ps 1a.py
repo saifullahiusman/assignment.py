92b5606d15bc7007221a2f03d780cbfa3c82afcd
saifullahiusman <54516091+saifullahiusman@users.noreply.github.com>
2019-09-15 18:53:25 +0100

    Python 1a
    Python assignment

---
+++ ps 1a.py
@@ -0,0 +1,5 @@
+555#PERCENTAGE OF MY DREAM HOME
+annual_salary = float(input("Enter your salary:"))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal:"))
+total_cost = float(input("Enter the cost of your dream home:"))
+
